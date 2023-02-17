0cbf08de38f2b1d4f12f4f442ace0225494b19e6
Jumuse <justinehoguet@gmail.com>
2023-02-17 08:12:11 +0100

    Linking class names to js for re using

---
+++ script.js
@@ -0,0 +1,14 @@
+var player1 = document.querySelector("h1.Player1");
+var player2 = document.querySelector("h1.Player2");
+let player1Turn = true;
+let player2Turn;
+let round1 = document.querySelector('.round1');
+let global1 = document.querySelector('.globalOne');
+let round2 = document.querySelector('.round2');
+let global2 = document.querySelector('.globalTwo');
+let rollBtn = document.querySelector('.roll');
+let dotRight = document.getElementById('dotRight');
+let dotLeft = document.getElementById('dotLeft');
+let currentTemp = 0;
+let currentGlobal = 0;
+let scoreTable = [0, 0];
